3458a07ea3a0a1086d18c454b4ef06bbcc7a5af0
Steven Boada <steven.boada@activision.com>
2025-10-29 13:57:15 -0600

    black

--- main.py
+++ main.py
@@ -11,35 +11,52 @@ app = Flask(__name__)
 upload_directory = os.path.abspath("./battlescribe")
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def upload_file():
     # adding file filter for mobile requests
     accept = 'accept = ".ros, .rosz"'
-    user_agent = request.headers.get('User-Agent', '').lower()
-    is_mobile = 'mobile' in user_agent or 'android' in user_agent
+    user_agent = request.headers.get("User-Agent", "").lower()
+    is_mobile = "mobile" in user_agent or "android" in user_agent
     if is_mobile:
         accept = ""
 
-    if request.method == 'POST':
-        f = request.files['file']
+    if request.method == "POST":
+        f = request.files["file"]
         file_ext = pathlib.Path(f.filename).suffix
 
         if file_ext == ".ros" or file_ext == ".rosz":
             random_filename = str(uuid.uuid4()) + file_ext
             f.save(os.path.join(upload_directory, random_filename))
-            json_phase, json_units, json_stratagems = battle_parse.parse_battlescribe(random_filename, request.form)
+            json_phase, json_units, json_stratagems = battle_parse.parse_battlescribe(
+                random_filename, request.form
+            )
             html_phase = prepare_html.convert_to_table(json_phase)
-            html_units = prepare_html.convert_to_table(json_units)
+            # Handle multi-detachment: json_units is a list of dicts
+            if isinstance(json_units, list):
+                html_units = "".join(
+                    [
+                        prepare_html.convert_units_to_divs(units_dict)
+                        for units_dict in json_units
+                        if isinstance(units_dict, dict)
+                    ]
+                )
+            else:
+                html_units = prepare_html.convert_units_to_divs(json_units)
             html_stratagems = prepare_html.convert_to_stratagem_list(json_stratagems)
-            return render_template("report.html", html_phase=html_phase, html_units=html_units, html_stratagems=html_stratagems)
+            return render_template(
+                "report.html",
+                html_phase=html_phase,
+                html_units=html_units,
+                html_stratagems=html_stratagems,
+            )
         else:
             return render_template("upload.html", accept=accept)
 
-    if request.method == 'GET':
+    if request.method == "GET":
         return render_template("upload.html", accept=accept)
 
 
-@app.route('/about', methods=['GET', 'POST'])
+@app.route("/about", methods=["GET", "POST"])
 def about_html():
     return render_template("about.html")
 
@@ -47,5 +64,5 @@ def about_html():
 wahapedia_db.init_db()
 battle_parse.init_parse()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=False)
